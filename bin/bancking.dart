8d7b0e101f49e88a90805176a42feeaaffddacf8
nora <nori@gmail.com>
2025-04-21 15:35:31 +0300

    Bancking App

---
+++ bin/bancking.dart
@@ -0,0 +1,5 @@
+import 'package:bancking/bancking.dart' as bancking;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bancking.calculate()}!');
+}
---
+++ test/bancking_test.dart
@@ -0,0 +1,8 @@
+import 'package:bancking/bancking.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
